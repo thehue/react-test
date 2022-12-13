87c376feab7a8b406be8d3974bee9ca47fe58419
thehue <gouk32@naver.com>
2022-12-13 21:53:46 +0900

    [TEST] Add color change button test

--- color-button/src/App.tsx
+++ color-button/src/App.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("red");
+
+  function onClickButton(): void {
+    setColor("blue");
+  }
+
   return (
     <div>
-      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+      <button style={{ backgroundColor: `${color}` }} onClick={onClickButton}>
+        Change to {color === "red" ? "blue" : "red"}
+      </button>
     </div>
   );
 }

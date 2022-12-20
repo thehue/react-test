ef8a25aedd997be4c50df2b2dc74444780371bac
thehue <gouk32@naver.com>
2022-12-20 21:57:32 +0900

    feat: add unchecked checkbox

--- color-button/src/App.tsx
+++ color-button/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       <button style={{ backgroundColor: buttonColor }} onClick={onClickButton}>
         Change to {newButtonColor}
       </button>
+      <input type="checkbox" />
     </div>
   );
 }

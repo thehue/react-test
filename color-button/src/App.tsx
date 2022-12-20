99eb965d9bbeeb237561efeaa155bd93958d24fb
thehue <gouk32@naver.com>
2022-12-20 22:19:42 +0900

    feat: add checkbox functionality

--- color-button/src/App.tsx
+++ color-button/src/App.tsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   function onClickButton(): void {
     setButtonColor(newButtonColor);
   }
 
+  const onChangeCheckbox = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { checked } = event.target;
+    if (checked) {
+      setButtonDisabled(true);
+      return;
+    }
+
+    setButtonDisabled(false);
+  };
+
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }} onClick={onClickButton}>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={onClickButton}
+        disabled={buttonDisabled}
+      >
         Change to {newButtonColor}
       </button>
-      <input type="checkbox" />
+      <input type="checkbox" onChange={onChangeCheckbox} />
     </div>
   );
 }
